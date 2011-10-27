9b3ddda4e6714e55139fbc2132842a27d0014486
Nazgum <nazgum@gmail.com>
2011-10-27 19:23:57 -0400

    dom_id update

--- lib/dom_id.rb
+++ lib/dom_id.rb
@@ -1,6 +1,6 @@
 class ActiveRecord::Base
   def dom_id(prefix = nil) 
     prefix ||= 'new' unless id
-    [ prefix, self.class.name.underscore, id ].compact * '_'
+    [ prefix, self.class.name.underscore.gsub("/", "_"), id ].compact * '_'
   end
 end
